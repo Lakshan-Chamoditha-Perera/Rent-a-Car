11fb12cf2fc11c953d955cacba126bfcbb52d693
Thenuk De Silva <desilva.thenuk@gmail.com>
2023-09-26 20:53:37 +0530

    UI forms initiated

--- src/main/java/lk/ijse/carhire_new1/controller/MainFormController.java
+++ src/main/java/lk/ijse/carhire_new1/controller/MainFormController.java
@@ -14,6 +14,11 @@ import java.io.IOException;
 public class MainFormController {
     public Button customerButton;
     public AnchorPane node;
+    public Button userButton;
+    public Button carButton;
+    public Button rentButton;
+    public Button logoutButton;
+    public AnchorPane root;
 
 
     public void customerAction(ActionEvent actionEvent) throws IOException {
@@ -22,5 +27,33 @@ public class MainFormController {
         this.node.getChildren().clear();
         this.node.getChildren().add(root);
 
+    }
+
+    public void userAction(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/view/user_form.fxml"));
+
+        this.node.getChildren().clear();
+        this.node.getChildren().add(root);
+    }
+
+    public void carAction(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/view/car_form.fxml"));
+
+        this.node.getChildren().clear();
+        this.node.getChildren().add(root);
+    }
+
+    public void rentAction(ActionEvent actionEvent) {
+    }
+
+    public void logoutAction(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/view/login_form.fxml"));
+
+        Scene scene = new Scene(root);
+
+        Stage stage = (Stage) this.root.getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Dashboard");
+        stage.centerOnScreen();
     }
 }
---
+++ src/main/java/lk/ijse/carhire_new1/controller/UserFormController.java
@@ -0,0 +1,19 @@
+package lk.ijse.carhire_new1.controller;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class UserFormController {
+    public AnchorPane root;
+    public TextField userIdText;
+    public TextField userNameText;
+    public TextField userAddressText;
+    public TextField userMobileText;
+    public TextField userNicText;
+    public Button deleteButton;
+    public Button updateButton;
+    public Button saveButton;
+    public TableView userTable;
+}
