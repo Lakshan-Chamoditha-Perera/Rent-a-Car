16289789a4669ce5fbb6209e7edb0b47a3899d68
Shan <perera.alc2000@gmail.com>
2023-09-27 22:07:30 +0530

    service factory

--- src/main/java/lk/ijse/carhire_new1/service/custom/impl/CustomerServiceImpl.java
+++ src/main/java/lk/ijse/carhire_new1/service/custom/impl/CustomerServiceImpl.java
@@ -1,10 +1,10 @@
 package lk.ijse.carhire_new1.service.custom.impl;
 
+import lk.ijse.carhire_new1.dao.custom.CustomerDao;
 import lk.ijse.carhire_new1.dto.CustomerDto;
 import lk.ijse.carhire_new1.service.custom.CustomerService;
 
 public class CustomerServiceImpl implements CustomerService{
-
     @Override
     public Boolean save(CustomerDto dto) {
         return null;
---
+++ src/main/java/lk/ijse/carhire_new1/service/util/ServiceFactory.java
@@ -0,0 +1,4 @@
+package lk.ijse.carhire_new1.service.util;
+
+public class ServiceFactory {
+}
