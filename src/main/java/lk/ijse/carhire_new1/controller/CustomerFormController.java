ca5e154aa0db58e0ae72ba576df60e18d9fbf9ec
Shan <perera.alc2000@gmail.com>
2023-09-27 21:52:34 +0530

    initialize() in Customer Controller

--- src/main/java/lk/ijse/carhire_new1/controller/CustomerFormController.java
+++ src/main/java/lk/ijse/carhire_new1/controller/CustomerFormController.java
@@ -1,11 +1,15 @@
 package lk.ijse.carhire_new1.controller;
 
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
-public class CustomerFormController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CustomerFormController implements Initializable {
     public TextField custIdText;
     public TextField custTitleText;
     public TextField custNameText;
@@ -17,4 +21,9 @@ public class CustomerFormController {
     public TextField mobileText;
     public TextField addressText;
     public AnchorPane root;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
