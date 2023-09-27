16db355d9b38d4ceccd71145b794d402b54b98ba
Shan <perera.alc2000@gmail.com>
2023-09-27 22:16:43 +0530

    dependancy injected in CustomerServiceImpl

--- src/main/java/lk/ijse/carhire_new1/service/custom/impl/CustomerServiceImpl.java
+++ src/main/java/lk/ijse/carhire_new1/service/custom/impl/CustomerServiceImpl.java
@@ -2,9 +2,17 @@ package lk.ijse.carhire_new1.service.custom.impl;
 
 import lk.ijse.carhire_new1.dao.custom.CustomerDao;
 import lk.ijse.carhire_new1.dto.CustomerDto;
+import lk.ijse.carhire_new1.service.ServiceFactory;
 import lk.ijse.carhire_new1.service.custom.CustomerService;
+import lk.ijse.carhire_new1.service.util.ServiceType;
 
-public class CustomerServiceImpl implements CustomerService{
+public class CustomerServiceImpl implements CustomerService {
+    private final CustomerDao customerDao;
+
+    public CustomerServiceImpl() {
+        customerDao = ServiceFactory.getInstance().getService(ServiceType.Customer_Service);
+    }
+
     @Override
     public Boolean save(CustomerDto dto) {
         return null;
